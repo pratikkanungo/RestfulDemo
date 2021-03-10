faa29aa98dd966b6a791558390ac129f96a6c2f6
pratikkanungo <31898590+pratikkanungo@users.noreply.github.com>
2021-03-10 18:56:35 +0400

    Update vag-setup.sh
    changes done for the maven repo

--- vag-setup.sh
+++ vag-setup.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
-sudo apt-get update
+sudo yum update
 echo "Installing Git.."
-sudo apt-get install -y git
-echo "Installing Maven.."
-sudo apt-get purge maven maven2 maven3
-sudo apt-add-repository ppa:andrei-pozolotin/maven3
-sudo apt-get update
-sudo apt-get install maven3
-sudo apt-get install -y software-properties-common python-software-properties
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo yum install -y git
+
+
+
 echo "Installing Java 8.."
-#sudo add-apt-repository -y ppa:webupd8team/java
-#sudo apt-get update
-#sudo apt-get install -y oracle-java8-installer
-#echo "Setting environment variables for Java 8.."
-#sudo apt-get install -y oracle-java8-set-default
-#export MAVEN_OPTS='-Xmx512m -XX:MaxPermSize=128m'
-sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt-get update
-sudo apt-get install openjdk-8-jdk
-sudo update-alternatives --config java
-sudo update-alternatives --config javac
+
+sudo yum install -y java-1.8.0-openjdk-devel
+
+
+echo "Installing Maven.."
+sudo yum install -y wget
+
+wget  http://www.eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz
+tar xzf apache-maven-3.3.9-bin.tar.gz
+mkdir /usr/local/maven
+mv apache-maven-3.3.9/ /usr/local/maven/
+alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-3.3.9/bin/mvn 1
+alternatives --config mvn
+
 git clone https://github.com/pratikkanungo/RestfulDemo.git
 cd RestfulDemo
 mvn spring-boot:run
